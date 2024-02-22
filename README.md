8c2f63e727126ff4c513c40ac3c9a301362c25b8
manikantaPitla <pitlamanikanta81@gmail.com>
2024-02-22 17:02:48 +0530

    News Today

---
+++ README.md
@@ -0,0 +1,38 @@
+# News Today Web Application
+
+The "News Today" web application allows users to browse and read news articles from various categories and countries. Here's an overview of its features, design, and implementation:
+
+## Features
+
+1. **Search**: Users can search for specific news articles using the search bar.
+2. **Country Selection**: Users can select their country to view top headlines from that country.
+3. **Article Categories**: Users can browse news articles based on different categories such as General, Business, Technology, Entertainment, Sports, Science, and Health.
+4. **Responsive Design**: The web application is designed to be responsive, providing a seamless experience across different devices.
+
+## Design
+
+- **Typography**: The website uses serif and sans-serif fonts for a classic and modern look.
+- **Color Scheme**: The color scheme includes shades of black, white, and blue, providing a clean and professional appearance.
+- **Layout**: The layout is structured with navigation at the top, followed by search and country selection options. News articles are displayed in a grid format below these sections.
+
+## Responsive Design
+
+<details>
+  <summary>Mobile Device</summary>
+
+![Mobile Screenshot](./images/home-sm.png)
+
+</details>
+
+<details>
+  <summary>Desktop Device</summary>
+  
+![Desktop Screenshot](./images/home-lg.png)
+</details>
+
+## Implementation
+
+- **API Integration**: The application integrates with the News API to fetch real-time news data based on user interactions.
+- **Dynamic Content**: News articles are dynamically generated and displayed on the webpage based on user actions such as country selection and category selection.
+- **Error Handling**: Error messages are displayed to users in case of any issues with fetching news data.
+- **Event Handling**: Various user interactions such as clicking on news categories and search are handled using event listeners to trigger appropriate actions.
--- script.js
+++ script.js
@@ -1,151 +1,132 @@
-const API_KEY = "a0d825f8e1ce4741b03067a47838cfa8";
-// const API_KEY = "1a83dd93a2024e5dafc4096878f9aa83";
-// const API_KEY = "3d585ef3352247d783f9add830b0014c";
+const API_KEY = "ce7e3dcbeae94143a42aa8ef5b1b6a29";
+const countrySelect = document.getElementById("countrySelect");
+const newsContainer = document.getElementById("newsContainer");
+const searchIcon = document.getElementById("searchIcon");
+const inputText = document.getElementById("inputText");
+const requestError = document.getElementById("requestError");
+const loadingContainer = document.getElementById("loadingContainer");
 
-let countrySelect = document.getElementById("countrySelect");
 let country = countrySelect.value;
-
-let selectedOptionText = countrySelect.options[countrySelect.selectedIndex].textContent;
+let selectedOptionText =
+  countrySelect.options[countrySelect.selectedIndex].textContent;
 let query = selectedOptionText.replace(/\s/g, "");
+let apiUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
 
+function fetchNews(url) {
+  newsContainer.innerHTML = "";
+  loadingContainer.classList.remove("d-none");
 
-function articleCard(articles){
-    let {author, content, description, publishedAt, source,title, url, urlToImage} = articles;
-    let {name} = source;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Error while fetching");
+      }
+      return response.json();
+    })
+    .then((jsonData) => {
+      jsonData.articles.forEach((article) => articleCard(article));
+      loadingContainer.classList.add("d-none");
+      errorRender("none");
+    })
+    .catch((error) => {
+      errorRender("flex", error.message);
+      loadingContainer.classList.add("d-none");
+      console.error("Error:", error.message);
+    });
+}
 
-    let newsContainer = document.getElementById("newsContainer");
+function articleCard(article) {
+  const {
+    author,
+    content,
+    description,
+    publishedAt,
+    source,
+    title,
+    url,
+    urlToImage,
+  } = article;
+  const date = new Date(publishedAt);
+  const formattedDate = `${date.getDate()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear()}`;
 
-    let articleCardContainer = document.createElement("div");
-    articleCardContainer.classList.add("article-card-container");
-    newsContainer.appendChild(articleCardContainer);
-
-
-    articleCardContainer.innerHTML = 
-    `<div class="article-image-container">
-        <img src="${urlToImage}" class="article-image">
+  const articleCardContainer = document.createElement("div");
+  articleCardContainer.classList.add("article-card-container");
+  articleCardContainer.innerHTML = `
+    <div class="article-image-container">
+      <img src="${
+        urlToImage || "./images/default.jpg"
+      }" class="article-image" alt="No Image">
     </div>
     <div class="article-content-container">
-        <h1 class="article-title">${title}</h1>
-        <p class="article-content">
-            ${content}
-        </p>
-        <div class="article-details-container">
-            <a href="${url}" class="read-more-btn" target="__blank">
-                Read More
-            </a>
-            <p class="publisher-name">
-                ${name}
-            </p>
-            <p class="published-content">
-                Published At: ${publishedAt}
-            </p>
-        </div>
+      <h1 class="article-title">${title}</h1>
+      <p class="article-content">${content}</p>
+      <div class="article-details-container">
+        <a href="${url}" class="read-more-btn" target="__blank">Read More</a>
+        <p class="publisher-name">${source.name}</p>
+        <p class="published-content">Published At: ${formattedDate}</p>
+      </div>
     </div>
-    `
-
-    let hrLine = document.createElement("hr");
-    newsContainer.appendChild(hrLine);
+  `;
+  newsContainer.appendChild(articleCardContainer);
 }
 
-
-let  fetchNews = async (apiUrl) =>{
-    document.getElementById("newsContainer").innerHTML = "";
-    document.getElementById('loadingContainer').classList.remove('d-none');
-
-    try {
-        var req = new Request(apiUrl);
-        let response = await fetch(req);
-        if (!response.ok) {
-        throw new Error("Network response was not ok");
-        }
-        let jsonData = await response.json();
-        let articles = jsonData.articles;
-        for (let eachArticle of articles) {
-        articleCard(eachArticle);
-        }
-        document.getElementById('loadingContainer').classList.add('d-none');
-    } catch (e) {
-        console.log('Error:', e);
-    }
-  
+function errorRender(visibility, errorMsg = "") {
+  requestError.innerHTML = `<p>${errorMsg}</p>`;
+  requestError.style = `
+  display : ${visibility};
+  justify-content:center;
+  align-items:center;
+  height : 100%;
+  width : 100%;
+  font-size : 14px;
+  font-weight :500;
+  `;
 }
 
+function performSearch() {
+  const inputValue = inputText.value;
+  apiUrl = `https://newsapi.org/v2/everything?q=${inputValue}&apiKey=${API_KEY}`;
+  fetchNews(apiUrl);
+}
 
-function search() {
-    const searchIcon = document.getElementById("searchIcon");
-    const inputText = document.getElementById("inputText");
-  
-    function performSearch() {
-      let inputValue = inputText.value;
-      apiUrl = `https://newsapi.org/v2/everything?q=${inputValue}&apiKey=${API_KEY}`;
-      fetchNews(apiUrl);
-    }
-  
-    searchIcon.addEventListener("click", (e) => {
-      performSearch();
-    });
-  
-    inputText.addEventListener("keyup", (e) => {
-      if (e.key === "Enter") {
-        performSearch();
+searchIcon.addEventListener("click", performSearch);
+
+inputText.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    performSearch();
+  }
+});
+
+countrySelect.addEventListener("change", () => {
+  country = countrySelect.value;
+  selectedOptionText =
+    countrySelect.options[countrySelect.selectedIndex].textContent;
+  query = selectedOptionText.replace(/\s/g, "");
+  document.getElementById("countryName").textContent = selectedOptionText;
+  apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
+  fetchNews(apiUrl);
+});
+
+function articleCategories() {
+  const targetItems = document.querySelectorAll(".news-type-item");
+  targetItems.forEach((item) => {
+    item.onclick = (e) => {
+      category = e.target.textContent;
+
+      if (category === "General") {
+        apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
+      } else if (category === selectedOptionText) {
+        apiUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
+      } else {
+        apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}`;
       }
-    });
-}
-search();
-  
 
-
-function articleCategories(){  
-    const targetItems = document.querySelectorAll('.news-type-item');
-    targetItems.forEach(item => {
-        item.onclick = (e) => {
-            category = e.target.textContent;
-
-            if(category === "General"){
-                apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
-            }else if(category === selectedOptionText){
-                apiUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
-            }else{
-                apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}`
-            }
-
-            fetchNews(apiUrl);
-        };
-    });
+      fetchNews(apiUrl);
+    };
+  });
 }
 articleCategories();
 
-
-function countryChange(){
-
-    countrySelect.addEventListener("change", (e)=>{
-        country = countrySelect.value;
-
-        selectedOptionText = countrySelect.options[countrySelect.selectedIndex].textContent;
-        query = selectedOptionText.replace(/\s/g, "");
-
-        document.getElementById("countryName").textContent = selectedOptionText;
-        apiUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
-        fetchNews(apiUrl);
-    })
-}
-countryChange();
-
-// active tab change //
-const newsTypeItems = document.querySelectorAll(".news-type-item");
-
-newsTypeItems.forEach(item => {
-  item.addEventListener("click", () => {
-    const clickedIndex = Array.from(newsTypeItems).indexOf(item);
-    const activeIndex = Array.from(newsTypeItems).findIndex(item => item.classList.contains("active"));
-    if (activeIndex !== -1 && activeIndex !== clickedIndex) {
-      newsTypeItems[activeIndex].classList.remove("active");
-    }
-    item.classList.add("active");;
-  });
-});
-
-
-
-apiUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
 fetchNews(apiUrl);
